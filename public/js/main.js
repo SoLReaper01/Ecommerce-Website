ec512c1532694579ebca83d24d3fde6f38b8d3f1
Andrei David <andreikeanan.david.828@my.csun.edu>
2026-04-28 23:53:43 -0700

    Cleaned up main.js code

--- public/js/main.js
+++ public/js/main.js
@@ -9,31 +9,31 @@ if (page.includes("register.html")) {
   registerForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-  const form = e.target;
+    const form = e.target;
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const password = document.getElementById("password").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const password = document.getElementById("password").value;
 
-  const res = await fetch("http://localhost:3000/api/auth/register", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ name, email, phone, password })
-  });
+    const res = await fetch("http://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, email, phone, password })
+    });
 
-  const data = await res.json();
-  
-  if (res.ok) {
-    alert("User created");
+    const data = await res.json();
+    
+    if (res.ok) {
+        alert("User created");
+        form.reset();
 
-    form.reset();
-  } else {
-    alert(data.message || "Registration failed");
-  }
-});
+    } else {
+        alert(data.message || "Registration failed");
+    }
+    });
 }
 
 // Login Form
@@ -43,28 +43,29 @@ if (page.includes("login.html")) {
   loginForm.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-  const email = document.getElementById("loginEmail").value;
-  const password = document.getElementById("loginPassword").value;
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
 
-  const res = await fetch("http://localhost:3000/api/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    credentials: "include",
-    body: JSON.stringify({ email, password })
-  });
+    const res = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify({ email, password })
+    });
 
-  const data = await res.json();
+    const data = await res.json();
 
-  if (res.ok) {
-    console.log("LOGIN SUCCESS HIT");
-    alert("Logged in!");
-    window.location.href = "index.html";
-  } else {
-    alert(data.message || "Login failed");
-  }
-});
+    if (res.ok) {
+        console.log("LOGIN SUCCESS HIT");
+        alert("Logged in!");
+        window.location.href = "index.html";
+        
+    } else {
+        alert(data.message || "Login failed");
+    }
+    });
 }
 
 fetch("http://localhost:3000/api/auth/profile", {
