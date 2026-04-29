a48e35c11da47c71e5fb0f80ce28a65fe0a1da90
SoL <solreaper1928@gmail.com>
2026-04-28 22:01:00 -0700

    Default Admin and User

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS users CASCADE;
 
 --users table
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    --id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(20),
