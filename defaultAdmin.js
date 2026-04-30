de2856bd42fa93415c2807b80e3d209129c7adbd
Andrei David <andreikeanan.david.828@my.csun.edu>
2026-04-30 15:49:21 -0700

    Created a script to create a default admin account.

---
+++ defaultAdmin.js
@@ -0,0 +1,33 @@
+const pool = require("./server/db");
+const bcrypt = require("bcrypt");
+
+// Creates a default Admin account
+async function defaultAdmin() {
+  const email = "admin@shop.com";
+  const password = "admin123";
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Checks if the Admin account already exists
+  const existing = await pool.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email]
+  );
+
+  if (existing.rows.length > 0) {
+    console.log("Admin already exists");
+    return;
+  }
+
+  await pool.query(
+    `
+    INSERT INTO users (name, email, password_hash, role)
+    VALUES ($1, $2, $3, $4)
+    `,
+    ["Admin", email, hashedPassword, "admin"]
+  );
+
+  console.log("Admin created!");
+}
+
+defaultAdmin().then(() => process.exit());
